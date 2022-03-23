a9e6540cc540f1ad3fff3fb48973821ae174f973
Baktygul <baktygul.aliev@gmail.com>
2022-03-22 20:18:07 -0500

    updated

--- components/views/Order.js
+++ components/views/Order.js
@@ -77,6 +77,8 @@ export default () => html`
         />
         <label for="top5">red pepper</label>
       </div>
+      <input type="hidden" name="customer" id="customer" value="Bak" />
+
       <input type="submit" name="submit" value="Submit Pizza" />
     </form>
   </section>
